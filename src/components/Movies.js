233dd24ec7a37f1301dfdce0522255fc3c9fde5b
Victor <victormakanda254@gmail.com>
2023-11-02 21:50:28 +0300

    First commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  return (
+  <div>
+    <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return (
+        <div key={movie.title}>
+          <h1>Name:{movie.title}</h1>
+          <p>time: {movie.time}</p>
+
+          Genres:
+          <ul>
+            {movie.genres.map((genre, index) => <li key={index}>{genre}</li>)
+
+            }
+          </ul>
+          
+        </div>
+        )
+      })}
+    
+  
+  </div>
+  );
 }
 
 export default Movies;
